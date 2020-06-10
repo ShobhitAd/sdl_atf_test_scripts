31337a97f4d8557e13844433db095b34d5130be4
Dmitriy Boltovskiy <dboltovskyi@luxoft.com>
2020-06-10 16:07:19 -0400

    Update script for 2457 issue

--- test_scripts/Defects/5_0/2457_Needed_fixes_sending_of_UpdateDeviceList_notification_on_device_connect.lua
+++ test_scripts/Defects/5_0/2457_Needed_fixes_sending_of_UpdateDeviceList_notification_on_device_connect.lua
@@ -3,34 +3,77 @@
 --
 -- Description:
 -- Steps to reproduce:
--- 1) HMI and SDL started, connect device.
+-- 1) HMI and SDL started, connect mobile device.
 -- Expected:
--- 1) SDL has to notify system with BC.UpdateDeviceList on device connect even if device does not have any SDL-enabled applications running.
+-- 1) SDL has to notify system with BC.UpdateDeviceList on device connect
+-- even if device does not have any SDL-enabled applications running.
 ---------------------------------------------------------------------------------------------------
 -- [[ Required Shared libraries ]]
 local runner = require('user_modules/script_runner')
 local common = require('user_modules/sequences/actions')
-local utils = require ('user_modules/utils')
+local utils = require("user_modules/utils")
+local SDL = require("SDL")
+
+--[[ Conditions to skip test ]]
+if config.defaultMobileAdapterType ~= "TCP" then
+  runner.skipTest("Test is applicable only for TCP connection")
+end
 
 -- [[ Test Configuration ]]
 runner.testSettings.isSelfIncluded = false
 
 -- [[ Local function ]]
-local function start ()
-    common.start()
-    common.getHMIConnection():ExpectRequest("BasicCommunication.UpdateDeviceList",
-        { deviceList = { { id = utils.getDeviceMAC(), name = utils.getDeviceName()} } })
-    common.getHMIConnection():ExpectRequest("BasicCommunication.OnDeviceAdded")
-    :Times(0)
+local function getDeviceList()
+  local connection = common.mobile.getConnection()
+  local weDevice = {
+    id = utils.buildDeviceMAC("WS"),
+    name = "Web Engine",
+    transportType = "WEBENGINE_WEBSOCKET"
+  }
+  local deviceList = {
+    [1] = {
+      id = utils.getDeviceMAC(connection.host, connection.port),
+      name = utils.getDeviceName(connection.host, connection.port),
+      transportType = "WIFI"
+    }
+  }
+  if SDL.buildOptions.webSocketServerSupport == "ON" then
+    table.insert(deviceList, 1, weDevice)
   end
+  return deviceList
+end
+
+local function startWOMobile()
+  local event = common.run.createEvent()
+  common.init.SDL()
+  :Do(function()
+      common.init.HMI()
+      :Do(function()
+        common.init.HMI_onReady()
+          :Do(function()
+              common.hmi.getConnection():RaiseEvent(event, "Start event")
+            end)
+        end)
+    end)
+  return common.hmi.getConnection():ExpectEvent(event, "Start event")
+end
+
+local function connectMobile()
+  common.init.connectMobile()
+  utils.printTable(getDeviceList())
+  common.getHMIConnection():ExpectRequest("BasicCommunication.UpdateDeviceList", {
+    deviceList = getDeviceList()
+  })
+end
 
 --[[ Scenario ]]
 runner.Title("Preconditions")
 runner.Step("Clean environment", common.preconditions)
+runner.Step("Start SDL and HMI", startWOMobile)
 
 -- [[ Test ]]
 runner.Title("Test")
-runner.Step("Start SDL, HMI, connect Mobile", start)
+runner.Step("Connect Mobile", connectMobile)
 
 -- [[ Postconditions ]]
 runner.Title("Postconditions")
